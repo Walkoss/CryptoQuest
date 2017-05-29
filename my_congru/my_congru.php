1d70e53d8e051818bacedfe94eab6bfbea47c4bb
Walkoss <walkoss@users.noreply.github.com>
2017-05-29 12:53:24 +0200

    Removed useless tabs

--- my_congru/my_congru.php
+++ my_congru/my_congru.php
@@ -4,8 +4,8 @@ function my_congru($a, $b, $n)
 {
     if (!$n)
     {
-		echo "va t'acheter des doigts !\n";
-		return (-1);
+        echo "va t'acheter des doigts !\n";
+        return (-1);
     }
     else if (is_int(($a - $b) / $n))
         return (1);
