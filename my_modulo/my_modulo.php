98961395eae6c68884e2e7179d0db28acfb33b06
Walkoss <walkoss@users.noreply.github.com>
2017-05-29 10:27:10 +0200

    Added tabs instead of spaces

--- my_modulo/my_modulo.php
+++ my_modulo/my_modulo.php
@@ -1,14 +1,14 @@
 <?php
 
 function error() {
-    echo "va t'acheter des doigts !\n";
-    return (-1);
+	echo "va t'acheter des doigts !\n";
+	return (-1);
 }
 
 function my_modulo($int, $n) {
-    if (!is_int($int) || !is_int($n))
-        return (error());
-    else if (!$n)
-        return (error());
-    return $int - (intval($int / $n) * $n);
+	if (!is_int($int) || !is_int($n))
+		return (error());
+	else if (!$n)
+		return (error());
+	return $int - (intval($int / $n) * $n);
 }
