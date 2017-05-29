5d018f1a53347255a020ef1d8f99fa048d0f9a8c
Mudada <mudada@Mudadas-MacBook-Pro.local>
2017-05-29 10:40:01 +0200

    Done

---
+++ my_modulo_neg/my_modulo_neg.php
@@ -0,0 +1,16 @@
+<?php
+
+function error()
+{
+	echo "va t'acheter des doigts !\n";
+	return (-1);
+}
+
+function my_modulo($int, $n)
+{
+	if (!is_int($int) || !is_int($n))
+		return (error());
+	else if (!$n || $n > 0)
+		return (error());
+	return $int - (intval($int / $n) * $n);
+}
