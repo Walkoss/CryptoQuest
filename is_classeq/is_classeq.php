6e36a12528e5f32d1c5e71ef05bff2d8b4568b00
Walkoss <walkoss@users.noreply.github.com>
2017-05-31 23:29:45 +0200

    Fixed tests

--- is_classeq/is_classeq.php
+++ is_classeq/is_classeq.php
@@ -1,17 +1,5 @@
 <?php
 
-function my_congru($a, $b, $n)
-{
-    if (!$n || !is_int($a) || !is_int($b) || !is_int($n))
-    {
-        echo "va t'acheter des doigts !\n";
-        return (-1);
-    }
-    else if (is_int(($a - $b) / $n))
-        return (1);
-    return (0);
-}
-
 function is_classeq($tab, $x, $n)
 {
     if (!is_array($tab) || empty($tab))
@@ -28,7 +16,7 @@ function is_classeq($tab, $x, $n)
     }
     foreach ($tab as $value)
     {
-        $my_congru_result = my_congru2($value, $x, $n);
+        $my_congru_result = my_congru($value, $x, $n);
         if ($my_congru_result !== 1)
             return ($my_congru_result);
     }
