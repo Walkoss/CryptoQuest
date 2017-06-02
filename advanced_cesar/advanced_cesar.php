346aead3789526647c3679a2fd4b3f5eabc0e12b
Walkoss <walkoss@users.noreply.github.com>
2017-06-02 16:12:36 +0200

    Removed unused function

--- advanced_cesar/advanced_cesar.php
+++ advanced_cesar/advanced_cesar.php
@@ -1,32 +1,5 @@
 <?php
 
-function my_modulo2($int, $n)
-{
-	if (!is_int($int) || !is_int($n))
-	{
-		echo "va t'acheter des doigts !\n";
-		return ($int);
-	}
-	else if (!$n)
-	{
-		echo "va t'acheter des doigts !\n";
-		return ($int);
-	}
-	while ($int < 0 || $int >= abs($n))
-	{
-		if (($int < 0 && $n < 0) || ($int > 0 && $n > 0))
-			$int -= $n;
-		else
-			$int += $n;
-	}
-	return ($int);
-}
-
-function remove_accent2($str)
-{
-    return iconv('utf-8', 'ASCII//TRANSLIT//IGNORE', $str);
-}
-
 function encrypt_string2($str, $n, $alpha)
 {
     $alphabet= array();
